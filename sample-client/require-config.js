a06a866e530eaa041d511840c730beca58180d9d
nosix <i.nosix@gmail.com>
2017-07-31 19:15:20 +0900

    add lodash and vue.js sample

--- sample-client/require-config.js
+++ sample-client/require-config.js
@@ -1,8 +1,8 @@
 var require = {
     baseUrl: 'build/js',
     paths: {
-        lodash: 'https://unpkg.com/lodash@4.13.1/lodash.min',
-        axios: 'https://unpkg.com/axios@0.12.0/dist/axios.min',
+        lodash: 'https://cdn.jsdelivr.net/lodash/4.17.4/lodash',
+        axios: 'https://cdn.jsdelivr.net/axios/0.16.1/axios',
     },
     enforceDefine: true,
 };
--- sample-client/src/main/SampleClient.kt
+++ sample-client/src/main/SampleClient.kt
@@ -1,3 +1,22 @@
+import kotlin.js.Json
+import kotlin.js.json
+
+@JsNonModule
+@JsModule("lodash")
+external val lodash: dynamic
+
+@JsNonModule
+@JsModule("vue")
+external class Vue(option: Json)
+
 fun run() {
-    sayHello()
+    println(lodash.capitalize("hello world"))
+
+    val vm: dynamic = Vue(json(
+            "el" to "#example",
+            "data" to json(
+                    "message" to "Hello World"
+            )
+    ))
+    vm.message = "Hello Kotlin World"
 }
